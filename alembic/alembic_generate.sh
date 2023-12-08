20fc3d2fd82c9ecaa5249d9534e5d12753d6c7b4
niv13 <niv@dcentralab.com>
2023-12-08 19:44:27 +0200

    fix alembic scripts

--- alembic/alembic_generate.sh
+++ alembic/alembic_generate.sh
@@ -2,6 +2,6 @@
 echo "Generating................
 -"
 cd /usr/local/bin
-echo "alembic -c alembic/alembic_\"\$1\".ini revision --autogenerate -m \"\$2\"" > alembic_generate
+echo "alembic -c alembic_\"\$1\".ini revision --autogenerate -m \"\$2\"" > alembic_generate
 chmod 777 alembic_generate
 echo "Generation for 'alembic_generate' completed!"
--- alembic/alembic_upgrade.sh
+++ alembic/alembic_upgrade.sh
@@ -2,6 +2,6 @@
 echo "Generating................
 -"
 cd /usr/local/bin
-echo "alembic -c alembic/alembic_\"\$1\".ini upgrade head" > alembic_upgrade
+echo "alembic -c alembic_\"\$1\".ini upgrade head" > alembic_upgrade
 chmod 777 alembic_upgrade
 echo "Generation for 'alembic_upgrade' completed!"
