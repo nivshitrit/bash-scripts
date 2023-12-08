76c1c9d8d8168174c7892afc2c6efabb49355f15
niv13 <niv@dcentralab.com>
2023-12-08 20:26:13 +0200

    test

---
+++ git/push2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Generating...
+cd /usr/local/bin
+echo "git rm --cache \"*.pyc\"
+args=(\"\$@\")
+git add .
+git commit -m \"\$args\"
+git push" > push
+chmod 777 push
+echo Generation for 'push' completed!
+
